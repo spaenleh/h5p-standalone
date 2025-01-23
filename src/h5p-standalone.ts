b8f171e3a0c4300e9bd0d58720c02fefde636d62
spaenleh <spaenleh@gmail.com>
2025-01-23 14:51:59 +0100

    fix: add back changes from upstream

--- src/h5p-standalone.ts
+++ src/h5p-standalone.ts
@@ -234,12 +234,14 @@ export class H5PStandalone {
 
     const mainLibrary = await this.findMainLibrary(
       H5PJsonContent,
-      librariesPath
+      librariesPath,
+      options?.assetsRequestFetchOptions
     );
 
     const dependencies = await this.findAllDependencies(
       H5PJsonContent,
-      librariesPath
+      librariesPath,
+      options?.assetsRequestFetchOptions
     );
 
     let { styles, scripts } = this.sortDependencies(
@@ -308,6 +310,7 @@ export class H5PStandalone {
     H5PIntegration.url = contentId;
     H5PIntegration.urlLibraries = librariesPath;
     H5PIntegration.postUserStatistics = !!options.postUserStatistics;
+    H5PIntegration.reportingIsEnabled = !!options.reportingIsEnabled;
 
     //since the default is false, only set if it's a number?
     if (options.saveFreq && typeof options.saveFreq === "number") {
@@ -490,7 +493,8 @@ export class H5PStandalone {
    */
   async findMainLibrary(
     h5pJsonContent: H5PPackageDefinition,
-    librariesPath: string
+    librariesPath: string,
+    assetsRequestFetchOptions?: RequestInit
   ): Promise<H5PLibraryDefinition> {
     const mainLibraryInfo = h5pJsonContent.preloadedDependencies.find(
       (dependency) => dependency.machineName === h5pJsonContent.mainLibrary
@@ -498,7 +502,10 @@ export class H5PStandalone {
 
     const mainLibraryFolderName = this.libraryToFolderName(mainLibraryInfo);
     const libraryFileUrl = `${librariesPath}/${mainLibraryFolderName}/library.json`;
-    return getJSON<H5PLibraryDefinition>(libraryFileUrl);
+    return getJSON<H5PLibraryDefinition>(
+      libraryFileUrl,
+      assetsRequestFetchOptions
+    );
   }
 
   /**
@@ -507,7 +514,8 @@ export class H5PStandalone {
    */
   async findAllDependencies(
     h5pJsonContent: H5PPackageDefinition,
-    librariesPath
+    librariesPath: string,
+    assetsRequestFetchOptions?: RequestInit
   ): Promise<LocalLibraryDependency[]> {
     const directDependencyFolderNames: string[] =
       h5pJsonContent.preloadedDependencies.map((dependency) =>
@@ -517,7 +525,8 @@ export class H5PStandalone {
     return this.loadDependencies(
       directDependencyFolderNames,
       [],
-      librariesPath
+      librariesPath,
+      assetsRequestFetchOptions
     );
   }
 
@@ -534,13 +543,18 @@ export class H5PStandalone {
   async loadDependencies(
     toFind: string[],
     alreadyFound: LocalLibraryDependency[],
-    librariesPath: string
+    librariesPath: string,
+    assetsRequestFetchOptions?: RequestInit
   ): Promise<LocalLibraryDependency[]> {
     const dependencies = alreadyFound;
     const findNext = [];
 
     const dependenciesDiscoveryRequests = toFind.map((libraryFolderName) => {
-      return this.findLibraryDependencies(libraryFolderName, librariesPath);
+      return this.findLibraryDependencies(
+        libraryFolderName,
+        librariesPath,
+        assetsRequestFetchOptions
+      );
     });
 
     const discoveredDependencies = await Promise.all(
@@ -570,7 +584,12 @@ export class H5PStandalone {
     });
 
     if (findNext.length > 0) {
-      return this.loadDependencies(findNext, dependencies, librariesPath);
+      return this.loadDependencies(
+        findNext,
+        dependencies,
+        librariesPath,
+        assetsRequestFetchOptions
+      );
     }
     return dependencies;
   }
@@ -584,10 +603,14 @@ export class H5PStandalone {
    */
   async findLibraryDependencies(
     libraryFolderName: string,
-    librariesPath: string
+    librariesPath: string,
+    assetsRequestFetchOptions?: RequestInit
   ): Promise<LocalLibraryDependency> {
     const libraryFileUrl = `${librariesPath}/${libraryFolderName}/library.json`;
-    const library = await getJSON<H5PLibraryDefinition>(libraryFileUrl);
+    const library = await getJSON<H5PLibraryDefinition>(
+      libraryFileUrl,
+      assetsRequestFetchOptions
+    );
 
     let dependencies: string[] = [];
     if (library.preloadedDependencies) {
